b4fe6b6621bebec502f6c852a648f9087db8ee53
rajesh karka <rajeshkarka@rajeshs-MacBook-Pro.local>
2019-12-08 11:47:34 -0700

    intial project structure

---
+++ src/main/java/com/intuit/demo/boot/appointment/BootUpAppointment.java
@@ -0,0 +1,13 @@
+package com.intuit.demo.boot.appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootUpAppointment {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootUpAppointment.class, args);
+	}
+
+}
